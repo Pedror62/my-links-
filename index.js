1b58f9cb9dff5f81156004eef33ed3bb180feb8e
pedror62 <102195011+Pedror62@users.noreply.github.com>
2023-02-23 10:42:34 -0300

    atualização js
    foi feito a aplicação interativa de acordo com o momento em que e acessado o my-links.

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+  function Saudacao() {
+    var mensagem = document.getElementById("mensagem");
+    
+    var horaAtual = new Date().getHours();
+    var saudacao;
+    ///var horaAtual = 12
+  
+  
+    if (horaAtual >= 5 && horaAtual < 12) {
+      saudacao = "Olá, Bom dia! &#9925";
+     // document.boby.style.background = `#e2cd9f`
+     document.getElementById('container').style.backgroundColor = '#e2cd9f';
+      
+    } else if (horaAtual >= 12 && horaAtual < 18) {
+      saudacao = "Olá, Boa tarde! &#127774";
+      document.getElementById(`container`).style.backgroundColor = `#b9846f`
+     
+    } else {
+      saudacao = "Olá, Boa noite! &#127769";
+      document.getElementById(`container`).style.backgroundColor = `#515154`
+    
+    }
+  
+    mensagem.innerHTML = saudacao ;
+  
+   
+  }
